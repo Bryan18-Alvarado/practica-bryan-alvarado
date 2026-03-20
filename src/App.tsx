8c8148e872b6682eb4ff8052005ccd44e8aeb855
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-03-19 20:43:41 -0600

    additing commit explaining exercise 3 and 4

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <div className="flex flex-col justify-center items-center h-svh">
       {/* <UserList /> */}
       {/* <GradeCalculator /> */}
+      {/* //TODO: el tercer ejercicio es el mismo componente de abajo, solo movi la logica al hook y lo llame a como pedia */}
       <StatusToggle />
     </div>
   )
